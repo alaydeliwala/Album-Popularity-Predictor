a8a7e80b54d46cd70dc5f4477d9df791ec2bce10
Alay Deliwala <ahdeliwala@gmail.com>
2019-10-17 12:07:46 -0400

    added inf

--- data_compiler.py
+++ data_compiler.py
@@ -5,15 +5,13 @@ import pandas as pd
 def main():
     album_df = pd.read_csv('data/billboard_album_data.csv')
     album_features_df  = pd.read_csv('data/album_acoustic_features.csv')
-    # output_df = pd.DataFrame(columns=['album', 'artist','year','acousticness_mean','acousticness_var','danceability_mean','danceability_var','energy_mean','energy_var','instrumentalness_mean','instrumentalness_var','liveness_mean','liveness_var','loudness_mean','loudness_var','speechiness_mean','speechiness_var','tempo_mean','tempo_var','rank'])
-    output_df = pd.DataFrame()
+    output_df = pd.DataFrame(columns=['album', 'artist','year','acousticness_mean','acousticness_var','danceability_mean','danceability_var','energy_mean','energy_var','instrumentalness_mean','instrumentalness_var','liveness_mean','liveness_var','loudness_mean','loudness_var','speechiness_mean','speechiness_var','tempo_mean','tempo_var','rank'])
     match_count=0
     for album,artist,year,rank in zip(album_df['album_name'],album_df['artist'],album_df['year'],album_df['rank']):
         if album_features_df.loc[album_features_df['album'] == album].size == 0:
             pass
         else:
-            match_count += 1
-            temp_df = pd.DataFrame([[
+            temp_df = [
                 album,
                 artist,
                 year,
@@ -34,14 +32,13 @@ def main():
                 album_features_df.loc[album_features_df['album'] == album].values[0][15],
                 album_features_df.loc[album_features_df['album'] == album].values[0][16],
                 rank
-                ]])
-            # print(output_df.append(temp_df))
-            
-        # print()
+                ]
+
+            output_df.loc[match_count] = temp_df
+            match_count += 1
         
-    print(match_count)
-    # output_df.columns=[['album', 'artist','year','acousticness_mean','acousticness_var','danceability_mean','danceability_var','energy_mean','energy_var','instrumentalness_mean','instrumentalness_var','liveness_mean','liveness_var','loudness_mean','loudness_var','speechiness_mean','speechiness_var','tempo_mean','tempo_var','rank']]
-    output_df.to_csv('data/final.csv')
+    print("Number of entries that found a match: " + str(match_count))
+    output_df.to_csv('data/full_album_data.csv')
 
     
 if (__name__ == "__main__"):
