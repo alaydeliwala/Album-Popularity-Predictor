ac8335fe9ce1fff2125a7408a32d3e0890e5f52d
Arpit Patel <ampatel7@ncsu.edu>
2019-10-29 18:54:47 -0400

    Standardized and Normalized predictions for decision tree and svm

--- svm_model.py
+++ svm_model.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 from scipy import stats
 import matplotlib.pyplot as plt
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
 
 train_df = pd.read_csv('data/train_data.csv',
@@ -44,39 +44,23 @@ for i in sel_pre:
         mean_train = np.mean(x_train, axis=0)
         std_train = np.std(x_train, axis=0)
         z_test = (x_test - mean_train) / std_train
-    
-gini_norm = DecisionTreeClassifier(criterion = "gini", random_state = 100) # max_depth, min_samples_leaf
-gini_std = DecisionTreeClassifier(criterion = "gini", random_state = 100) # max_depth, min_samples_leaf
 
-gini_norm.fit(norm_train, y_train)
-gini_std.fit(z_train, y_train)
+svm_norm = SVC()
+svm_std = SVC()
 
-entropy_norm = DecisionTreeClassifier(criterion = "entropy", random_state = 100)# max_depth, min_samples_leaf
-entropy_std = DecisionTreeClassifier(criterion = "entropy", random_state = 100)# max_depth, min_samples_leaf
+svm_norm.fit(norm_train, y_train)
+svm_std.fit(z_train, y_train)
 
-entropy_norm.fit(norm_train, y_train)
-entropy_std.fit(z_train, y_train)
+norm_svm_rank_pred = svm_norm.predict(x_test)
+std_svm_rank_pred = svm_std.predict(x_test)
 
-rank_pred_gini_norm = gini_norm.predict(x_test)
-rank_pred_gini_std = gini_std.predict(x_test)
-
-rank_pred_entropy_norm = entropy_norm.predict(x_test)
-rank_pred_entropy_std = entropy_std.predict(x_test)
-
-y_pred_gini_norm_hit = [1 if x <= hit_rating else 0 for x in rank_pred_gini_norm]
-y_pred_gini_std_hit = [1 if x <= hit_rating else 0 for x in rank_pred_gini_std]
-
-y_pred_entropy_norm_hit = [1 if x <= hit_rating else 0 for x in rank_pred_entropy_norm]
-y_pred_entropy_std_hit = [1 if x <= hit_rating else 0 for x in rank_pred_entropy_std]
+y_pred_svm_norm_hit = [1 if x <= hit_rating else 0 for x in norm_svm_rank_pred]
+y_pred_svm_std_hit = [1 if x <= hit_rating else 0 for x in std_svm_rank_pred]
 
 y_test_hit = [1 if x <= hit_rating else 0 for x in test_df['rank']]
 
-norm_gini_accuracy = accuracy_score(y_test_hit, y_pred_gini_norm_hit) * 100
-std_gini_accuracy = accuracy_score(y_test_hit, y_pred_gini_std_hit) * 100
-norm_entropy_accuracy = accuracy_score(y_test_hit, y_pred_entropy_norm_hit) * 100
-std_entropy_accuracy = accuracy_score(y_test_hit, y_pred_entropy_std_hit) * 100
+norm_svm_accuracy = accuracy_score(y_test_hit, y_pred_svm_norm_hit) * 100
+std_svm_accuracy = accuracy_score(y_test_hit, y_pred_svm_std_hit) * 100
 
-print('Normalized Gini Accuracy: ', norm_gini_accuracy)
-print('Standardized Gini Accuracy: ', std_gini_accuracy)
-print('Normalized Entropy Accuracy: ', norm_entropy_accuracy)
-print('Standardized Entropy Accuracy: ', std_entropy_accuracy)
+print('Normalized SVM Accuracy: ', norm_svm_accuracy)
+print('Standardized SVM Accuracy: ', std_svm_accuracy)
