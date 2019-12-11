f8cab5c738d28b124be80f38ec215347cb8c571f
Arpit Patel <ampatel7@ncsu.edu>
2019-12-10 22:32:37 -0500

    Added dataset downlaod script and commented code in decision tree

---
+++ dataset_download.sh
@@ -0,0 +1,4 @@
+mkdir data/
+cd data/
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1BqxIklysgCJXq1SJsX-Jlw98e2mg56wf' -O- | sed -rm 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1BqxIklysgCJXq1SJsX-Jlw98e2mg56wf" -O train_data.csv && rm -rf /tmp/cookies.txt
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1h8agdS6_3DLGEzrue7rsxR7Z8kr4A8Gz' -O- | sed -rm 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1h8agdS6_3DLGEzrue7rsxR7Z8kr4A8Gz" -O test_data.csv && rm -rf /tmp/cookies.txt
--- decision_tree.py
+++ decision_tree.py
@@ -4,7 +4,9 @@ from scipy import stats
 import matplotlib.pyplot as plt
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
+from sklearn import tree
 
+# Reading in the datasets from the data file
 train_df = pd.read_csv('data/train_data.csv',
                        usecols=[
                            'acousticness_mean', 'danceability_mean',
@@ -19,64 +21,110 @@ test_df = pd.read_csv('data/test_data.csv',
                           'liveness_mean', 'loudness_mean', 'speechiness_mean',
                           'tempo_mean', 'rank'
                       ])
-                      
+                
+# Global Variable for hit_rating his is the threshold for a hit album
 hit_rating = 25
 
+# Split x and y from the training data set
 x_train = np.array(train_df.loc[:, train_df.columns[:-1]])
 y_train = np.array(train_df.loc[:, train_df.columns[-1]])
 
+# Split x and y from the test data set
 x_test = np.array(test_df.loc[:, test_df.columns[:-1]])
 y_test = np.array(test_df.loc[:, test_df.columns[-1]])
 
-pre_dict = {0: 'Normalization', 1: 'Standardization'}
-sel_pre = [0, 1]
-for i in sel_pre:
-    if pre_dict[i] == 'Normalization':
-        norm_train = (x_train - x_train.min(axis=0)) / (x_train.max(axis=0) -
-                                                        x_train.min(axis=0))
-        # Use the min and max of training data to normalize testing data
-        min_train = np.min(x_train, axis=0)
-        max_train = np.max(x_train, axis=0)
-        norm_test = (x_test - min_train) / (max_train - min_train)
-    elif pre_dict[i] == 'Standardization':
-        z_train = stats.zscore(x_train)
-        # Use the min and max of training data to standardize testing data
-        mean_train = np.mean(x_train, axis=0)
-        std_train = np.std(x_train, axis=0)
-        z_test = (x_test - mean_train) / std_train
-    
-gini_norm = DecisionTreeClassifier(criterion = "gini", random_state = 100) # max_depth, min_samples_leaf
+# Standardize the training data set
+z_train = stats.zscore(x_train)
+# Use the min and max of training data to standardize testing data
+mean_train = np.mean(x_train, axis=0)
+std_train = np.std(x_train, axis=0)
+z_test = (x_test - mean_train) / std_train
+
+# Create the Gini Index Tree Classifier
 gini_std = DecisionTreeClassifier(criterion = "gini", random_state = 100) # max_depth, min_samples_leaf
 
-gini_norm.fit(norm_train, y_train)
+# Fit the gini index model to the standardized data set
 gini_std.fit(z_train, y_train)
 
-entropy_norm = DecisionTreeClassifier(criterion = "entropy", random_state = 100)# max_depth, min_samples_leaf
+# Create the Entropy Tree Classifier
 entropy_std = DecisionTreeClassifier(criterion = "entropy", random_state = 100)# max_depth, min_samples_leaf
 
-entropy_norm.fit(norm_train, y_train)
+# Fit the entropy model to the standardized data set
 entropy_std.fit(z_train, y_train)
 
-rank_pred_gini_norm = gini_norm.predict(x_test)
-rank_pred_gini_std = gini_std.predict(x_test)
+# Make the ranked predictions for the gini model
+rank_pred_gini_std = gini_std.predict(z_test)
 
-rank_pred_entropy_norm = entropy_norm.predict(x_test)
-rank_pred_entropy_std = entropy_std.predict(x_test)
+# Make the ranked predictions for the entropy model
+rank_pred_entropy_std = entropy_std.predict(z_test)
 
-y_pred_gini_norm_hit = [1 if x <= hit_rating else 0 for x in rank_pred_gini_norm]
+# Reclassify predictions and test ranks to hits or not hits
 y_pred_gini_std_hit = [1 if x <= hit_rating else 0 for x in rank_pred_gini_std]
 
-y_pred_entropy_norm_hit = [1 if x <= hit_rating else 0 for x in rank_pred_entropy_norm]
 y_pred_entropy_std_hit = [1 if x <= hit_rating else 0 for x in rank_pred_entropy_std]
 
 y_test_hit = [1 if x <= hit_rating else 0 for x in test_df['rank']]
 
-norm_gini_accuracy = accuracy_score(y_test_hit, y_pred_gini_norm_hit) * 100
+# Calculate the accuracy scores
 std_gini_accuracy = accuracy_score(y_test_hit, y_pred_gini_std_hit) * 100
-norm_entropy_accuracy = accuracy_score(y_test_hit, y_pred_entropy_norm_hit) * 100
 std_entropy_accuracy = accuracy_score(y_test_hit, y_pred_entropy_std_hit) * 100
 
-print('Normalized Gini Accuracy: ', norm_gini_accuracy)
+# Print the accuracies for the Gini and Entropy decision trees
 print('Standardized Gini Accuracy: ', std_gini_accuracy)
-print('Normalized Entropy Accuracy: ', norm_entropy_accuracy)
 print('Standardized Entropy Accuracy: ', std_entropy_accuracy)
+
+#Create the Gini Index Accuracy Graph
+for datapoints in range(2, 16):
+    gini_std = DecisionTreeClassifier(criterion = "gini", random_state = 100,
+        min_samples_split = datapoints)
+        
+    gini_std.fit(z_train, y_train)
+    
+    rank_pred_gini_std = gini_std.predict(z_test)
+    
+    y_test_hit = [1 if x <= hit_rating else 0 for x in test_df['rank']]
+    y_pred_gini_std_hit = [1 if x <= hit_rating else 0 for x in rank_pred_gini_std]
+    sum = 0
+    for point in range(len(y_test_hit)):
+        if(y_pred_gini_std_hit[point] == y_test_hit[point]):
+            sum += 1
+    avg = sum/len(y_test_hit)
+    plt.plot((datapoints), avg,
+             'ro')
+             
+plt.title("GINI Index Accuracy w/ Standardized Data")
+plt.xlabel('Datapoints Needed to Split')
+plt.ylabel('Accuracy')
+plt.savefig('output/GINI_Accuracy_with_Standardized_Data.png')
+plt.clf()
+
+#Create the Entropy Accuracy Graph
+for datapoints in range(2, 16):
+    entropy_std = DecisionTreeClassifier(criterion = "entropy", random_state = 100, min_samples_split = datapoints)
+    
+    entropy_std.fit(z_train, y_train)
+
+    rank_pred_entropy_std = entropy_std.predict(z_test)
+
+    y_test_hit = [1 if x <= hit_rating else 0 for x in test_df['rank']]
+
+    y_pred_entropy_std_hit = [1 if x <= hit_rating else 0 for x in rank_pred_entropy_std]
+    sum = 0
+    for point in range(len(y_test_hit)):
+        if(y_pred_entropy_std_hit[point] == y_test_hit[point]):
+            sum += 1
+    avg = sum/len(y_test_hit)
+    plt.plot((datapoints), avg,
+             'ro')
+    
+plt.title("Entropy Accuracy W/ Standardized Data")
+plt.xlabel('Datapoints Needed to Split')
+plt.ylabel('Accuracy')
+plt.savefig('output/Entropy_Accuracy_with_Standardized_Data.png')
+plt.clf()
+
+
+
+
+    
+
